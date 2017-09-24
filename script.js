026b5548ddd0934c1f03b39d1882b9dbc5ae12a4
akash444 <akash.saraf444@gmail.com>
2017-09-23 22:39:58 -0700

    yup
    final

---
+++ script.js
@@ -0,0 +1,43 @@
+function myfun(){
+	var name = document.getElementById("fname").value;
+	alert("Thank You " + name+ " for your inquiry, we will contact you soon");
+}
+function hoverin1(){
+	document.getElementById("icn1").src="photo/fb1.png";
+}
+
+function hoverout1(){
+	document.getElementById("icn1").src="photo/fb.png";
+}
+
+function hoverin2(){
+	document.getElementById("icn2").src="photo/twt1.png";
+}
+
+function hoverout2(){
+	document.getElementById("icn2").src="photo/twt.png";
+}
+
+function hoverin3(){
+	document.getElementById("icn3").src="photo/insta1.png";
+}
+
+function hoverout3(){
+	document.getElementById("icn3").src="photo/insta.png";
+}
+
+function hoverin4(){
+	document.getElementById("icn4").src="photo/lin1.png";
+}
+
+function hoverout4(){
+	document.getElementById("icn4").src="photo/lin.png";
+}
+
+function hoverin5(){
+	document.getElementById("icn5").src="photo/you1.png";
+}
+
+function hoverout5(){
+	document.getElementById("icn5").src="photo/you.png";
+}
